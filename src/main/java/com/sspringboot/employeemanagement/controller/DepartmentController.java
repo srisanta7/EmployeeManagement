b75433ae8883ed0184257d80b01ba7fd76ac6f9e
srisanta7 <srisantapradhan7@gmail.com>
2025-11-26 16:11:01 +0530

    second commit

---
+++ src/main/java/com/sspringboot/employeemanagement/controller/DepartmentController.java
@@ -0,0 +1,7 @@
+package com.sspringboot.employeemanagement.controller;
+
+public class DepartmentController {
+          void dept() {
+        	  System.out.println("in department");
+          }
+}
