44d2fe70572efa9f97a628e233b6f21749f8ea87
Srisanta Pradhan <srisantapradhan7@gmail.com>
2025-11-27 10:40:03 +0530

    Update DepartmentController.java

--- src/main/java/com/sspringboot/employeemanagement/controller/DepartmentController.java
+++ src/main/java/com/sspringboot/employeemanagement/controller/DepartmentController.java
@@ -4,5 +4,7 @@ public class DepartmentController {
           void dept() {
         	 int i = 5+70;
         	 System.out.println(i+"is the value");
+             String s = "hello";
+             System.out.println(s+" welcome to sspringboot");
           }
 }
