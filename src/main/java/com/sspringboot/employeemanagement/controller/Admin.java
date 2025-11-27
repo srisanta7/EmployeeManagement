1015a7c6cdb31aac843d96801acbc7123ee3ab53
Srisanta Pradhan <srisantapradhan7@gmail.com>
2025-11-27 10:47:45 +0530

    adding admin
    added admin controller

---
+++ src/main/java/com/sspringboot/employeemanagement/controller/Admin.java
@@ -0,0 +1,10 @@
+package com.sspringboot.employeemanagement.controller;
+
+public class Admin {
+            void admin() {
+            	 int i = 10+20;
+            	 System.out.println(i+" is the admin value");
+                 String s = "admin";
+                 System.out.println(s+" welcome to sspringboot admin panel");
+            }
+}
